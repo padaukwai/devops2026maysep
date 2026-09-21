dd426057a161a4130f5197440938797fbf66b56e
padaukwai <waimarhlaing86@gmail.com>
2026-09-22 01:18:52 +0630

    mysql db added in pom.xml

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,33 +1,193 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
+
+import java.sql.*;
 
 public class App
 {
+    private Connection con = null;
+
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+    public void displayEmployee(Employee emp)
+    {
+        System.out.println("display function");
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27017
-        //MongoClient mongoClient = new MongoClient("localhost", 27000);
-        // Connect to MongoDB
-        //after building network bridge(mongo-dbserver is container name)
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "Software Engineering Methods")
-                .append("year", "2021")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
-        collection.insertOne(doc);
+//        try
+//        {
+//            // Load Database driver
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//        }
+//        catch (ClassNotFoundException e)
+//        {
+//            System.out.println("Could not load SQL driver");
+//            System.exit(-1);
+//        }
+//
+//        // Connection to the database
+//        Connection con = null;
+//        int retries = 5;
+//        for (int i = 0; i < retries; ++i)
+//        {
+//            System.out.println("Connecting to database...");
+//            try
+//            {
+//                // Wait a bit for db to start
+//                Thread.sleep(50000);
+//                // Connect to database
+//                //if you run java on windows
+//              //  con = DriverManager.getConnection("jdbc:mysql://localhost:33060/employees?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
+//                //if you run java on docker compose
+//                //db:3306
+//                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
+//                System.out.println("Successfully connected");
+//                // Wait a bit
+//                Thread.sleep(10000);
+//                // Exit for loop
+//                break;
+//            }
+//            catch (SQLException sqle)
+//            {
+//                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+//                System.out.println(sqle.getMessage());
+//            }
+//            catch (InterruptedException ie)
+//            {
+//                System.out.println("Thread interrupted? Should not happen.");
+//            }
+//        }
+//
+//        if (con != null)
+//        {
+//            try
+//            {
+//                // Close connection
+//                con.close();
+//            }
+//            catch (Exception e)
+//            {
+//                System.out.println("Error closing connection to database");
+//            }
+//        }
+        // Create new Application
+        /*App a = new App();
 
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+        // Connect to database
+        a.connect();
+        System.out.println("After connecting");
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
+        // Disconnect from database
+        a.disconnect();*/
+        try {
+            App a = new App();
+            a.connect();
+            System.out.println("After connecting");
+            Employee emp = a.getEmployee(255530);
+            a.displayEmployee(emp);
+            a.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
